57142aa3b1f9a51740f30a3d9cbc2fba581b01e6
Tiago <tiagopinfari@gmail.com>
2025-11-18 15:31:09 -0300

    Primer commit

---
+++ src/main/java/com/example/examenmercado/ExamenmercadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenmercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenmercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenmercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examenmercado/ExamenmercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenmercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenmercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
